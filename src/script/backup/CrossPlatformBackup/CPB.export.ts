ce17b14d7931bb5837728a8a40a469997d2fae12
v-gira <virgile.girardin@gmail.com>
2025-06-26 14:52:13 +0200

    runfix: map audio assets to backup library specs

--- src/script/backup/CrossPlatformBackup/CPB.export.ts
+++ src/script/backup/CrossPlatformBackup/CPB.export.ts
@@ -176,7 +176,7 @@ export const exportCPBHistoryFromDatabase = async ({
           return;
         }
 
-        const metaData = buildMetaData(assetParseData.content_type, assetParseData.info);
+        const metaData = buildMetaData(assetParseData.content_type, assetParseData.info, assetParseData.meta);
 
         const asset = new BackupMessageContent.Asset(
           assetParseData.content_type,
